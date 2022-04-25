0d6d44eda9199cc5cf5e06afaf51ede74af13d44
smhuarahuara <77683748+smhuarahuara@users.noreply.github.com>
2022-04-25 06:15:47 -0400

    Create index.php

---
+++ pregunta1/index.php
@@ -0,0 +1,61 @@
+<?php include "miestiloinicio.inc.php"?> 
+        <!-- Banner -->
+        <section id="banner">
+									<div class="content">
+										<header>
+											<h1>"FACULTAD DE CIENCIAS PURAS Y NATURALES"<br /></h1>
+											<p>Universidad Mayor de San Andres</p>
+										</header>
+										<p style="text-align: justify;">La Facultad de Ciencias Puras Naturales tiene las siguientes modalidades de ingreso de acuerdo a las normativas universitarias: Prueba de suficiencia académica (PSA), Curso preuniversitario (CPU), Modalidad especial: olimpiadas (local, departamental, nacional e internacional), mejores estudiantes de municipios, personas con capacidades especiales. En sus 6 carreras: Biología, Estadística, Física, Informática, Matemática y Cs. Químicas.<br/>
+                	                     El curso Preuniversitario tiene como objetivos: <br/>
+										</p>
+                                        <ol>
+                                            <li><p>Recordar, actualizar y fortalecer conocimientos de materias básicas (adquiridas en el ciclo secundario) necesarias para el desarrollo formativo en la Facultad.</p></li>
+                                            <li><p>Fortalecer aptitudes, actitudes y habilidades para realizar los estudios superiores.</p></li>
+                                            <li><p>Introducir al conocimiento de métodos didácticos y técnicas de investigación y de estudio.</p></li>
+                                            <li><p>Inducir al conocimiento de la estructura y funcionamiento de la Universidad, la Facultad y la Carrera de su elección.</p></li>
+                                            <li><p>Contribuir a superar el criterio individualista y memorístico del modelo tradicional del proceso de enseñanza-aprendizje, incentivando a que en el  conocimiento se integre teoría y práctica.</p></li>
+                                            <li><p>Generar un espíritu crítico y productivo de la realidad social e institucional.</p></li>
+                                            <li><p>Atender a los requerimientos de regiones, provincias o localidades del departamento de La Paz, cuando las condiciones de densidad de población, importancias relativa o demanda social asi lo justifiquen; a través de medios virtuales.</p></li>
+                                        </ol>
+
+										
+									</div>
+									<span class="">
+										<img src="images/fcpn.png"  width="200" heigth="200"/>
+									</span>
+			</section>
+ <!-- Section -->
+ <section>
+									<header class="major">
+										<h2>Caracteristicas de la plataforma</h2>
+									</header>
+									<div class="features">
+										<article>
+											<span class="icon fa-gem"></span>
+											<div class="content">
+												<h3>Una plataforma inteligente</h3>
+												<p>La plataforma presente es perteneciente a la UMSA la cual esta hecha para una mejor comunicacion entre docentes y estudiantes asi para lograr ver sus calificaciones y conocer acerca de nuestra Facultad.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon solid fa-paper-plane"></span>
+											<div class="content">
+												<h3>Seguridad</h3>
+												<p>Nos preocupamos por tu seguridad por lo cual nuestra plataforma cuenta con la seguridad requerida al momento de realizar el logueo correspondiente.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon solid fa-signal"></span>
+											<div class="content">
+												<h3>Fiabilidad</h3>
+												<p>La plataforma se encuentra en buena forma y constante mantenimineto.</p>
+											</div>
+										</article>
+									</div>
+								</section>
+						</div>
+					</div>    
+							
+        
+<?php include "miestilofin.inc.php"?>   
